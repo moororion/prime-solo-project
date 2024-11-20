5e2159e9886a84b8964765debb886883f28144bc
Orion Moor <moororion@gmail.com>
2024-11-19 19:22:18 -0600

    removed unneeded file

--- src/components/Fridge/Fridge.jsx
+++ src/components/Fridge/Fridge.jsx
@@ -1,6 +1,6 @@
 import React, { useEffect, useState } from 'react';
 import axios from 'axios';
-import './fridge.css';
+import './Fridge.css';
 
 function Fridge() {
   const [fridgeItems, setFridgeItems] = useState([]);
