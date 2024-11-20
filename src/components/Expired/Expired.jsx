b3d7d56f08158d2320d349a53ea4ce8208b474d9
Orion Moor <moororion@gmail.com>
2024-11-19 19:19:35 -0600

    removed unneeded file

--- src/components/Expired/Expired.jsx
+++ src/components/Expired/Expired.jsx
@@ -1,6 +1,6 @@
 import React, { useEffect, useState } from 'react';
 import axios from 'axios';
-import './expired.css';
+import './Expired.css';
 
 function Expired() {
   const [expiredItems, setExpiredItems] = useState([]);
