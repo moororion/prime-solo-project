8a43d4d127afccceae701b568204ba8c7d7de5c5
Orion Moor <moororion@gmail.com>
2024-11-19 19:24:37 -0600

    fixed text in pantry and fridge.jsx, capitlized theit css counterparts

--- src/components/Pantry/Pantry.jsx
+++ src/components/Pantry/Pantry.jsx
@@ -1,6 +1,6 @@
 import React, { useEffect, useState } from 'react';
 import axios from 'axios';
-import './pantry.css';
+import './Pantry.css';
 
 function Pantry() {
   const [pantryItems, setPantryItems] = useState([]);
